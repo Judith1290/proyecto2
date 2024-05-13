be0abe96c215864ccfc6435b9550b79dc9c5a884
ManuKuzan <136617152+ManuKuzan@users.noreply.github.com>
2024-05-13 09:39:40 -0600

    primeraparte

---
+++ proyecto1.js
@@ -0,0 +1,23 @@
+const formularioAcceso = document.querySelector(".RegistroDeFormulario");
+const acceder = document.querySelector("#acceder")
+acceder.addEventListener("click", function () {
+   const mail = document.querySelector("input[type='email']").value;
+   const contraseña = document.querySelector("input[type='password']").value;
+
+    console.log(mail + contraseña)
+    // Verificar que el correo electrónico y la contraseña no estén vacíos
+    if (!mail || !contraseña) {
+        return alert("Por favor, ingrese su correo electrónico y contraseña.");
+    }
+
+    const Users = JSON.parse(localStorage.getItem("users")) || []; 
+
+    const validUser = Users.find(user => user.mail === mail && user.contraseña === contraseña);
+
+    if (!validUser || validUser.contraseña !== contraseña) {
+        return alert("El usuario o la contraseña son incorrectos o no están registrados.");
+    }
+
+    alert('¡Bienvenido ' + validUser.name + '!');
+    window.location.href = "folder.html";
+})
